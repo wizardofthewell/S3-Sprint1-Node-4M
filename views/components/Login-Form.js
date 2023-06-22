f269900468a82e8e8fc388f949e2f9d5c6009d9b
Alex Singleton <alex.singleton@keyin.com>
2023-06-22 14:49:01 -0230

    login component created and implemented

--- views/components/Login-Form.js
+++ views/components/Login-Form.js
@@ -1,14 +1,15 @@
 const form = () => {
   return `<form id="login-form" method="post" action="/login">
-  <div>
+  <h2 class="title">Login</h2>
+  <div class="form">
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" required>
   </div>
-  <div>
+  <div class="form">
     <label for="password">Password:</label>
     <input type="password" id="password" name="password" required>
   </div>
-  <div>
+  <div class="login-button">
     <input type="submit" value="Login">
   </div>
 </form>
--- views/login.js
+++ views/login.js
@@ -11,7 +11,9 @@ const page = async (res) => {
   const header = `<header><h1>${name}</h1><nav><a href="/signup">No account?</a></nav></header>`;
   try {
     await res.writeHead(res.statusCode, { "Content-Type": "text/html" });
-    await res.end(`${head}<body>${header}${LoginForm.form()}</body>`);
+    await res.end(
+      `${head}<body>${header}<div id="box">${LoginForm.form()}</div></body>`
+    );
   } catch (error) {
     res.statusCode = 500;
     console.error(error.message);
