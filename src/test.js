a36a09e80c745eefe76ad05e7ec4ab818e4ebb5f
Alex Singleton <alex.singleton@keyin.com>
2023-06-29 13:52:33 -0230

    fart beanz

--- src/test.js
+++ src/test.js
@@ -36,3 +36,50 @@ function updateToken(argv) {
     });
   });
 }
+
+function newToken(userName, password, email, phone) {
+  if (global.DEBUG) console.log("token.newToken()");
+  let date = format(new Date(), "y-MM-dd HH:mm.ss");
+  let exp = add(parseISO(date), { days: 1 });
+  let tkn = crc32(`${userName}#${date}#${email}`).toString(36);
+  let access = true;
+  let newToken = {
+    created: date,
+    username: userName,
+    password: password,
+    email: email,
+    phone: phone,
+    token: tkn,
+    expires: exp,
+    confirmed: access,
+  };
+  // now, i was gonna use listTokens, but they serve different purpose. list posts to console.
+  // we need to read to a variable
+  fs.readFile("./json/tokens.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (global.DEBUG) {
+        console.log(JSON.parse(data));
+      }
+      let tokens = JSON.parse(data);
+      tokens.map((token) => {
+        // if token === passed uusername we want to overwrite the users old token
+        if (token.username === userName) {
+          token.created = date;
+          token.expires = exp;
+          token.token = tkn;
+          console.log(token);
+        } else {
+          fs.writeFileSync("./json/tokens.json", token, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Token saved successfully.");
+            }
+          });
+        }
+      });
+    }
+  });
+}
